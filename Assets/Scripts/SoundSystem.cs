2d2a81c2ca46e5241326b59e59a2786921743caa
KelpForest <janina_carvajal@outlook.com>
2026-01-31 11:19:58 +0100

    Started adding some clips

---
+++ Assets/Scripts/SoundSystem.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SoundSystem : MonoBehaviour
+{
+    public AudioClip maskOnSound;
+    public AudioClip takeOffSound;
+    public AudioClip pickUpItemSound;
+    public AudioClip moneySound;
+    
+}
