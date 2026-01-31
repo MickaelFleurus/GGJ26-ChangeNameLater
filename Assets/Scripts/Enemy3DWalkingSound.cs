b1ee1fc10ff38bd3794225d88bc7eab65dafe871
KelpForest <janina_carvajal@outlook.com>
2026-01-31 19:53:14 +0100

    monster walking

---
+++ Assets/Scripts/Enemy3DWalkingSound.cs
@@ -0,0 +1,45 @@
+using GLTFast.Schema;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class Enemy3DWalkingSound : MonoBehaviour
+{
+    [Header("Enemy 3D Sound")]
+    public AudioClip soundClip;
+    public float minDistance = 3f;   // Full volume within 3 meters
+    public float maxDistance = 9f;  // Silent beyond 15 meters
+    public float volume = 0.5f;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // clocks 3d sound
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = soundClip;
+        audioSource.loop = true;               // Always looping
+        audioSource.spatialBlend = 1f;         // Full 3D, importanto
+        audioSource.minDistance = minDistance;
+        audioSource.maxDistance = maxDistance;
+        audioSource.volume = volume;
+        audioSource.playOnAwake = true;
+
+
+        GameEvents.OnMaskEquipped += StartWalking;
+        GameEvents.OnMaskOff += StopWalking;
+        //Play on start
+       // audioSource.Play();
+    }
+
+    public void StopWalking()
+    {
+        audioSource.Stop();
+    }
+
+    public void StartWalking()
+    {
+        if (!audioSource.isPlaying)
+            audioSource.Play();
+    }
+
+}
