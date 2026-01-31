82f7f6c028c32fb7e7a38abb21146bb112469b20
Mickaël Fleurus <fleurusmickael@gmail.com>
2026-01-31 14:57:55 +0100

    Add raycast and the value being displayed when the crosshair is on the object.

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField] public UIDocument UIDocument;
+
+
+    private Label mHints;
+    private Label mAmountCollected;
+    private Label mLootValue;
+
+    void Start()
+    {
+        mHints = UIDocument.rootVisualElement.Q<Label>("Hints");
+        mAmountCollected = UIDocument.rootVisualElement.Q<VisualElement>("Collected").Q<Label>("Amount");
+        mLootValue = UIDocument.rootVisualElement.Q<Label>("ObjectValue");
+
+        mHints.visible = false;
+        mLootValue.visible = false;
+
+        mAmountCollected.text = "0";
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+        float rayDistance = 5f;
+
+        if (Physics.Raycast(ray, out hit, rayDistance))
+        {
+            IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+
+            if (interactable != null)
+            {
+                mLootValue.visible = true;
+                mLootValue.text = interactable.GetValue().ToString();
+                return;
+            }
+
+        }
+        mLootValue.visible = false;
+
+    }
+
+}
